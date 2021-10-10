74e66d40cedf96a15edd9298959db0b26394255c
David Sungaila <david.sungaila@arcor.de>
2021-10-11 01:06:46 +0200

    Added unit tests for new API

--- Tests/ApiTests.cs
+++ Tests/ApiTests.cs
@@ -2,6 +2,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using static PDFtoZPL.Conversion;
 
 namespace Tests
@@ -49,5 +51,58 @@ namespace Tests
         {
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => ConvertPdfPage(string.Empty, page: -1));
         }
+
+        [TestMethod]
+        public void PdfAllStreamNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => ConvertPdf((Stream)null!).ToList());
+        }
+
+        [TestMethod]
+        public void PdfAllStringNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => ConvertPdf((string)null!).ToList());
+        }
+
+        [TestMethod]
+        public void PdfAllByteArrayNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => ConvertPdf((byte[])null!).ToList());
+        }
+
+#if NETCOREAPP3_0_OR_GREATER
+        [TestMethod]
+        public async Task PdfAllAsyncStreamNullException()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () =>
+            {
+                await foreach (var zplCode in ConvertPdfAsync((Stream)null!))
+                {
+                }
+            });
+        }
+
+        [TestMethod]
+        public async Task PdfAllAsyncStringNullException()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () =>
+            {
+                await foreach (var zplCode in ConvertPdfAsync((string)null!))
+                {
+                }
+            });
+        }
+
+        [TestMethod]
+        public async Task PdfAllAsyncByteArrayNullException()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () =>
+            {
+                await foreach (var zplCode in ConvertPdfAsync((byte[])null!))
+                {
+                }
+            });
+        }
+#endif
     }
 }
