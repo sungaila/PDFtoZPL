a5896911689a2ab872c607eec2b49041a25d6915
David Sungaila <david.sungaila@arcor.de>
2022-01-29 16:51:44 +0100

    Update to PDFtoImage 2.0.0-preview3

--- PDFtoZPL/Conversion.cs
+++ PDFtoZPL/Conversion.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Text;
 using System.Threading;
@@ -17,32 +15,6 @@ namespace PDFtoZPL
     /// </summary>
     public static class Conversion
     {
-#if !NETCOREAPP3_0_OR_GREATER
-        static Conversion()
-        {
-            // .NET Framework needs some extra code to find and load SkiaSharp from the runtimes folder
-            var workingDir = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().GetName(false).CodeBase!).LocalPath)!;
-            var skiaSharpLibPath = Path.Combine(workingDir, "runtimes", Environment.Is64BitProcess ? "win-x64" : "win-x86", "native", "libSkiaSharp.dll");
-
-            LoadLibrary(skiaSharpLibPath);
-        }
-
-        /// <summary>Loads the specified module into the address space of the calling process.</summary>
-        /// <param name="lpLibFileName">
-        /// <para>The name of the module. This can be either a library module (a .dll file) or an executable module (an .exe file). The name specified is the file name of the module and is not related to the name stored in the library module itself, as specified by the <b>LIBRARY</b> keyword in the module-definition (.def) file. If the string specifies a full path, the function searches only that path for the module. If the string specifies a relative path or a module name without a path, the function uses a standard search strategy to find the module; for more information, see the Remarks. If the function cannot find the  module, the function fails. When specifying a path, be sure to use backslashes (\\), not forward slashes (/). For more information about paths, see <a href="https://docs.microsoft.com/windows/desktop/FileIO/naming-a-file">Naming a File or Directory</a>. If the string specifies a module name without a path and the file name extension is omitted, the function appends the default library extension .dll to the module name. To prevent the function from appending .dll to the module name, include a trailing point character (.) in the module name string.</para>
-        /// <para><see href="https://docs.microsoft.com/windows/win32/api//libloaderapi/nf-libloaderapi-loadlibraryw#parameters">Read more on docs.microsoft.com</see>.</para>
-        /// </param>
-        /// <returns>
-        /// <para>If the function succeeds, the return value is a handle to the module. If the function fails, the return value is NULL. To get extended error information, call <a href="/windows/desktop/api/errhandlingapi/nf-errhandlingapi-getlasterror">GetLastError</a>.</para>
-        /// </returns>
-        /// <remarks>
-        /// <para><see href="https://docs.microsoft.com/windows/win32/api//libloaderapi/nf-libloaderapi-loadlibraryw">Learn more about this API from docs.microsoft.com.</see></para>
-        /// </remarks>
-        [DllImport("Kernel32", ExactSpelling = true, EntryPoint = "LoadLibraryW", SetLastError = true)]
-        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-        private static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPWStr)] string lpLibFileName);
-#endif
-
         /// <summary>
         /// Converts a single page of a given PDF into ZPL code.
         /// </summary>
