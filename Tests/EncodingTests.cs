218b5ee35bcc45aacf8022e129cc351104562f07
David Sungaila <david.sungaila@arcor.de>
2022-06-17 18:04:16 +0200

    Added unit tests for graphicFieldOnly option

--- Tests/EncodingTests.cs
+++ Tests/EncodingTests.cs
@@ -45,5 +45,36 @@ namespace Tests
 
             Assert.AreEqual(expectedResult, zplResult.Replace("\n", string.Empty));
         }
+
+        [TestMethod]
+        [DataRow("SocialPreview.pdf", null)]
+        [DataRow("SocialPreview.pdf", BitmapEncodingKind.Hexadecimal)]
+        [DataRow("SocialPreview.pdf", BitmapEncodingKind.HexadecimalCompressed)]
+        [DataRow("SocialPreview.pdf", BitmapEncodingKind.Base64)]
+        [DataRow("SocialPreview.pdf", BitmapEncodingKind.Base64Compressed)]
+        [DataRow("SocialPreview.png", null)]
+        [DataRow("SocialPreview.png", BitmapEncodingKind.Hexadecimal)]
+        [DataRow("SocialPreview.png", BitmapEncodingKind.HexadecimalCompressed)]
+        [DataRow("SocialPreview.png", BitmapEncodingKind.Base64)]
+        [DataRow("SocialPreview.png", BitmapEncodingKind.Base64Compressed)]
+        [DataRow("Wikimedia_Commons_web.pdf", null)]
+        [DataRow("Wikimedia_Commons_web.pdf", BitmapEncodingKind.Hexadecimal)]
+        [DataRow("Wikimedia_Commons_web.pdf", BitmapEncodingKind.HexadecimalCompressed)]
+        [DataRow("Wikimedia_Commons_web.pdf", BitmapEncodingKind.Base64)]
+        [DataRow("Wikimedia_Commons_web.pdf", BitmapEncodingKind.Base64Compressed)]
+        public void ConvertWithEncodingsGraphicFieldOnly(string fileName, BitmapEncodingKind? encodingKind)
+        {
+            var expectedResult = _expectedResults[fileName][encodingKind ?? BitmapEncodingKind.HexadecimalCompressed];
+
+            using var fileStream = new FileStream(Path.Combine("Assets", fileName), FileMode.Open, FileAccess.Read);
+
+            var zplResult = fileName.EndsWith(".pdf")
+                ? ConvertPdfPage(fileStream, encodingKind: encodingKind ?? BitmapEncodingKind.HexadecimalCompressed, graphicFieldOnly: true)
+                : ConvertBitmap(fileStream, encodingKind: encodingKind ?? BitmapEncodingKind.HexadecimalCompressed, graphicFieldOnly: true);
+
+            var trimmedExpected = expectedResult.Remove(0, "^XA".Length);
+
+            Assert.AreEqual(trimmedExpected.Remove(trimmedExpected.Length - "^FS^XZ".Length), zplResult.Replace("\n", string.Empty));
+        }
     }
 }
