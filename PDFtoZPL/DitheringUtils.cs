293f906f10c775607b073f137380f16c3a8bef6a
David Sungaila <david.sungaila@arcor.de>
2023-09-06 01:24:14 +0200

    Add strong name to WebConverter

--- PDFtoZPL/DitheringUtils.cs
+++ PDFtoZPL/DitheringUtils.cs
@@ -2,9 +2,7 @@
 using System;
 using static PDFtoZPL.Conversion;
 
-#if NET6_0_OR_GREATER
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PDFtoZPL.WebConverter")]
-#endif
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PDFtoZPL.WebConverter, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd0018feed1ba4fac91744d868cb1bb8a4b55a97eec0e02e90a8d57db56bff5a32f03813b1a6c8a7ccd50eed8880be4e91ad281c9ee81fd4182de0905c0590019e9bf87a8f83ce93c09da9c4eaae5bd8aa63816c4a3bef8bd16bb358d3aed64cc2ec3ae17698c336b63df4fbd719dc13bd9a88d9fcfa87a2426d34db78d05fb7")]
 
 namespace PDFtoZPL
 {
